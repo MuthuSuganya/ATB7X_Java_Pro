75791fdb9dc15a45d74d96bcbf8d25fe62005f85
Suganya <ymsuganya@gmail.com>
2024-07-12 23:20:02 +0530

    Add pgm

--- src/Java_07_pgms/Lab_01.java
+++ src/Java_07_pgms/Lab_01.java
@@ -1,2 +1,9 @@
-package Java_07_pgms;public class Lab_01 {
+package Java_07_pgms;
+
+public class Lab_01
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World ");
+    }
 }
---
+++ src/Java_07_pgms/Lab_02.java
@@ -0,0 +1,11 @@
+package Java_07_pgms;
+
+public class Lab_02
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2/2);
+        System.out.println(2+2);
+    }
+
+}
