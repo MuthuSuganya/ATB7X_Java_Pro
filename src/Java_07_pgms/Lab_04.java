6a05ba878afb6149c0d18b3c6e6c1280cc16b175
Suganya <ymsuganya@gmail.com>
2024-07-13 10:17:13 +0530

    Arithmetic calc

---
+++ src/Java_07_pgms/Lab_04.java
@@ -0,0 +1,17 @@
+package Java_07_pgms;
+
+public class Lab_04
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        int c=2;
+        int d=a*b;
+        int e=b/2;
+        int f=a%c;
+        System.out.println("Value of d:" +d);
+        System.out.println("Value of e:" +e);
+        System.out.println("Value of f:" +f);
+    }
+}
