08c259dadc368b22cb1fd53ca7ebc8d4e4054959
Suganya <ymsuganya@gmail.com>
2024-07-19 22:40:07 +0530

    Arithmetic calc

---
+++ src/Java_07_pgms/Lab_comment.java
@@ -0,0 +1,11 @@
+package Java_07_pgms;
+
+import java.io.IOException;
+
+public class Lab_comment
+{
+    public static void main(String args[])
+    {
+       System.out.println("Single Line Comment");
+           }
+}
