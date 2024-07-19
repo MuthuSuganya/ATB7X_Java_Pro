4bc44b1b0ade1ff1bf3efeb1614ee3f18d6bfcbf
Suganya <ymsuganya@gmail.com>
2024-07-19 22:58:34 +0530

    Arithmetic calc

---
+++ src/Java_13_pgms/Lab_06_methods.java
@@ -0,0 +1,17 @@
+package Java_13_pgms;
+
+public class Lab_06_methods
+{
+    public void testValues(){
+
+        int a = 10;
+        int b = 5;;
+    }
+
+    public static void main(String[] args) {
+
+        Lab_06_methods d = new Lab_06_methods();
+        d.testValues();
+        //System.out.println("Value of a" + d);
+    }
+}
---
+++ src/Java_13_pgms/Lab_07_calc.java
@@ -0,0 +1,15 @@
+package Java_13_pgms;
+
+public class Lab_07_calc
+{
+    int a = 10;
+    int b = 20;
+
+    public static void main(String[] args)
+    {
+        Lab_07_calc cal = new Lab_07_calc(); //Execute this method
+        int c = cal.a; // assign c= value of a
+        int d = cal.b; // assigns d =value of b
+        System.out.println(c+d);//sum up both the values
+    }
+}
