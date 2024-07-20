20f068e892a59f90d34dccf31231035877991218
Suganya <ymsuganya@gmail.com>
2024-07-20 16:29:50 +0530

    Arithmetic calc

---
+++ src/Java_07_pgms/Lab_calculation.java
@@ -0,0 +1,13 @@
+package Java_07_pgms;
+
+public class Lab_calculation
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2%2);
+         }
+}
