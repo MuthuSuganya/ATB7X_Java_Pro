13e3a8d435bdb4473f833a5a058b55cc972ce3f3
Suganya <ymsuganya@gmail.com>
2024-07-20 16:35:43 +0530

    Arithmetic calc

--- src/Java_07_pgms/Lab_no_main.java
+++ src/Java_07_pgms/Lab_no_main.java
@@ -3,6 +3,7 @@ package Java_07_pgms;
 public class Lab_no_main {
     // Can I run class without a main method?
 // run option?
+    //Yes method. main gets executed without any output
     static public void main(String[] args) {
 
     }
