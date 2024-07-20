5af9c316e662c23596153ade3e1f20f7222dd24b
MuthuSuganya <ymsuganya@gmail.com>
2024-07-20 16:39:24 +0530

    Update Lab_no_main.java

--- src/Java_07_pgms/Lab_no_main.java
+++ src/Java_07_pgms/Lab_no_main.java
@@ -3,7 +3,11 @@ package Java_07_pgms;
 public class Lab_no_main {
     // Can I run class without a main method?
 // run option?
-    //Yes method. main gets executed without any output
+    //Yes- main gets executed without any output
+    // valid class name 
+    //with _Lab01 
+    //with Muthu_suganya
+    //with Suganya2024
     static public void main(String[] args) {
 
     }
